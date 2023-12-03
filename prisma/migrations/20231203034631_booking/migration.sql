720525265ff11dc539b89661c5da81646b85d16a
kinanasih <18221044@std.stei.itb.ac.id>
2023-12-03 10:46:42 +0700

    Updated Booking Model

---
+++ prisma/migrations/20231203034631_booking/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `idMember` on the `Booking` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Booking" DROP COLUMN "idMember";

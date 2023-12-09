679079359fbe6cf32f95ac08659c4dfec41b32c0
michellelim04 <18221052@std.stei.itb.ac.id>
2023-12-09 10:00:17 +0700

    cors update

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,46 @@ import memberRoutes from './controllers/memberController';
 import roomRoutes from './controllers/roomController';
 
 const app = express()
+const NODE_ENV = ( process.env.NODE_ENV || "PROD" ).toUpperCase();
 dotenv.config()
 const port = process.env.PORT || 5001;
 const database_url = process.env.DATABASE_URL;
 
+if ( NODE_ENV !== "PROD" && NODE_ENV !== "DEV" && NODE_ENV !== "TEST" ) throw new Error( NODE_ENV );
 if ( database_url === undefined ) throw new Error( "DATABASE_URL is not provided" )
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true,
-  optionSuccessStatus: 200,
-}
-
-app.use( cors( corsOptions ) )
+	origin: ['http://localhost:4173', 'https://smarthubcoworking.netlify.app/'],
+	credentials: true,
+	optionSuccessStatus: 200,
+  };
+  
+const whitelist = [ /.*localhost.*$/, "https://smarthubcoworking.netlify.app/" ];
+  
+app.use(cors({
+	origin: (origin, callback) => {
+	  if (NODE_ENV === "DEV" || origin === undefined) return callback(null, true);
+	  for (const originCheck of whitelist) {
+		if (typeof originCheck === 'string' && originCheck === origin) {
+		  return callback(null, true);
+		} else if (originCheck instanceof RegExp && originCheck.test(origin as string)) {
+		  return callback(null, true);
+		}
+	  }
+	  return callback(new Error("Not Allowed by CORS"));
+	},
+	credentials: true,
+	methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+	allowedHeaders: [
+	  "Authorization",
+	  "Origin",
+	  "Accept",
+	  "Content-Type",
+	  "X-Forwarded-For",
+	],
+	optionsSuccessStatus: 204,
+}));
+  
 app.use( express.json() )
 
 app.all( "/", ( req: Request, res: Response ) => {
